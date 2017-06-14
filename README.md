2143d5a6e76b6aa4d0b3a73c7819d1a1be144134
Vishal Raghav <hemvishalraghav94@gmail..com>
2017-06-14 13:36:55 +0530

    Added functionality for providing manual selector paths

--- README.md
+++ README.md
@@ -4,10 +4,19 @@ JS library to detect addition of nodes on web page
 # Usage
 use function:
 ```
-NodeDetect(timing,callback);
+NodeDetect(callback,selectors,timing);
 ```
 where
+**selectors(Optional- ["body *"] by default)** : list of selectors path inside
+which node detected is to be implemented.
 **timing(Optional- 0ms by default)** : time limit between the adjacent function calls or to throttle down the event capturing by an specified time. (in ms)
 
 **callback** : Function to execute when event is captured on detection of node 
 on web page.
+
+# Example
+```
+NodeDetect(function(){
+console.log("Node Detected");
+},["div","p","a"],3000);
+```
--- detect-node.js
+++ detect-node.js
@@ -1,64 +1,86 @@
-	(function () {
-		var limit = 0; //default time limit (if time parameter is not passed)
-		var nodeDetect = {
-			init: function (callback) {
-				var css = "@keyframes nodeInserted {} @-moz-keyframes nodeInserted{} @-webkit-keyframes nodeInserted {} @-ms-keyframes nodeInserted {} @-o-keyfames nodeInserted{} body * {animation: nodeInserted 0s; -webkit-animation: nodeInserted 0s; -moz-animation: nodeInserted 0s; -ms-animation: nodeInserted 0s; -o-animation: nodeInserted 0s;}";
-				this.appendCSS(css);
-  				window.addEventListener('animationstart', this.throttle(function(event) {
-		    		if (event.animationName == 'nodeInserted') {
-		       			 callback(event);
-		   		 	}
-				},limit), true);
-			},
-			appendCSS: function (css) {
-				var head = document.head || document.getElementsbyTagName('head')[0] , 
-				style = document.createElement('style');
+(function () {
 
-				style.type = 'text/css';
-				if(style.styleSheet) {
-					style.styleSheet.cssText = css;
-				} else {
-					style.appendChild(document.createTextNode(css));
+	//default time limit (if time parameter is not passed)
+	var limit = 0;
+
+	var nodeDetect = {
+		/**
+		 *
+		 * @param callback - function to execute when node is detected
+		 * @param selectors - list of selectors path inside which node detection is to be implemented
+         */
+		init: function (callback,selectors) {
+			var selectorsString = selectors.join(",");
+			var animationValues = "{animation: nodeInserted 0s; -webkit-animation: nodeInserted 0s; -moz-animation: nodeInserted 0s; -ms-animation: nodeInserted 0s; -o-animation: nodeInserted 0s;}";
+			var css = "@keyframes nodeInserted {} @-moz-keyframes nodeInserted{} @-webkit-keyframes nodeInserted {} @-ms-keyframes nodeInserted {} @-o-keyfames nodeInserted{} "+selectorsString+animationValues;
+			this.appendCSS(css);
+			window.addEventListener('animationstart', this.throttle(function(event) {
+				if (event.animationName == 'nodeInserted') {
+					callback(event);
 				}
-				head.appendChild(style);
-			},
-			throttle: function (callback,limit) {
-						var wait = false,count=0,flag=true;
-				return function () {
-					if (!wait) {
-						count = 0;
-						wait = true;
-						if(flag) {
-							callback.apply(null, arguments);
-						}
-						else{
-							count++;
-						}
-						var args = arguments;
-						setTimeout(function () {
-							wait = false;
-							if(count>0){
-								callback.apply(null, args);
-								flag = false;
-								setTimeout(function(){
-									flag=true;
-								},limit);
-							}
-							else
-								flag=true;
-							count = 0;
-						}, limit);
-					} else {
-						count++;
-						flag = true;
+			},limit), true);
+		},
+		/**
+		 *
+		 * @param css - css styling to be added to style tag
+         */
+		appendCSS: function (css) {
+			var head = document.head || document.getElementsbyTagName('head')[0] ,
+				style = document.createElement('style');
+			style.type = 'text/css';
+			if(style.styleSheet) {
+				style.styleSheet.cssText = css;
+			} else {
+				style.appendChild(document.createTextNode(css));
+			}
+			head.appendChild(style);
+		},
+		throttle: function (callback,limit) {
+			var wait = false,count=0,flag=true;
+			return function () {
+				if (!wait) {
+					count = 0;
+					wait = true;
+
+					if(flag) {
+						callback.apply(null, arguments);
 					}
+					else{
+						count++;
+					}
+					var args = arguments;
+					setTimeout(function () {
+						wait = false;
+						if(count>0){
+							callback.apply(null, args);
+							flag = false;
+							setTimeout(function(){
+								flag=true;
+							},limit);
+						}
+						else
+							flag=true;
+						count = 0;
+					}, limit);
+				} else {
+					count++;
+					flag = true;
 				}
 			}
 		}
+	};
 
-		window.NodeDetect = function (callback, time) {
-			limit = time || limit;
-			nodeDetect.init(callback);
+	window.NodeDetect = function (callback,selectors,time) {
+		limit = time || limit;
+		if( !selectors || !selectors.length) {
+			selectors = ["body *"];
 		}
-		return nodeDetect;
-	})();
+
+		if(!(selectors instanceof Array)) {
+			selectors = [selectors];
+		}
+		nodeDetect.init(callback, selectors);
+	};
+
+	return nodeDetect;
+})();
